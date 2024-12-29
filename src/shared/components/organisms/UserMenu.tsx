99c71cd58102b4880083723470109584d30c4e7b
NguyenDucTin <99305494+Yonko08042002@users.noreply.github.com>
2024-12-30 03:45:28 +0700

    feat/master:fix type TourStyle

--- src/shared/components/organisms/UserMenu.tsx
+++ src/shared/components/organisms/UserMenu.tsx
@@ -1,23 +1,23 @@
-'use client';
-import type { UserWithRoles } from 'application/use-cases/user';
+"use client";
+import type { UserWithRoles } from "application/use-cases/user";
 import {
   Avatar,
   AvatarFallback,
-  AvatarImage
-} from 'shared/components/atoms/avatar';
+  AvatarImage,
+} from "shared/components/atoms/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from 'shared/components/atoms/dropdown-menu';
-import { AlignJustify, ChevronDown, LogOutIcon, UserPen } from 'lucide-react';
-import { signOut } from 'next-auth/react';
-import { Button } from '../atoms/button';
-import Link from 'next/link';
-import { useTranslations } from 'next-intl';
+  DropdownMenuTrigger,
+} from "shared/components/atoms/dropdown-menu";
+import { ChevronDown, LogOutIcon, UserPen } from "lucide-react";
+import { signOut } from "next-auth/react";
+import { Button } from "../atoms/button";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 interface UserMenuProps {
   user: UserWithRoles;
@@ -29,48 +29,48 @@ export const UserMenu = ({ user }: UserMenuProps) => {
   };
   const t = useTranslations();
   return (
-    <div className='relative flex gap-2 items-center border-black border-[1px] p-1 rounded-full'>
+    <div className="relative flex gap-2 items-center border-black border-[1px] p-1 rounded-full">
       <DropdownMenu>
-        <DropdownMenuTrigger className='rounded-full'>
-          <Avatar className='flex items-center w-6 h-6'>
+        <DropdownMenuTrigger className="rounded-full">
+          <Avatar className="flex items-center w-6 h-6">
             <AvatarImage src={user.avatar!} alt={user.name!} />
             <AvatarFallback>{user.email?.slice(0, 2)}</AvatarFallback>
           </Avatar>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className='mt-5 rounded-xl '>
-          <DropdownMenuLabel className='flex justify-center items-center gap-3'>
-            {' '}
+        <DropdownMenuContent className="mt-5 rounded-xl ">
+          <DropdownMenuLabel className="flex justify-center items-center gap-3">
+            {" "}
             <Avatar>
               <AvatarImage src={user.avatar!} alt={user.name!} />
               <AvatarFallback>{user.email.slice(0, 2)}</AvatarFallback>
-            </Avatar>{' '}
-            {user.name}{' '}
+            </Avatar>{" "}
+            {user.name}{" "}
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem className='px-2 font-semibold flex gap-3 '>
-            <Link href='/profile'>
+          <DropdownMenuItem className="px-2 font-semibold flex gap-3 ">
+            <Link href="/profile">
               <Button
-                variant={'link'}
-                className='px-0 flex gap-1  w-full text-base text-black justify-start hover:no-underline'
+                variant={"link"}
+                className="px-0 flex gap-1  w-full text-base text-black justify-start hover:no-underline"
               >
-                <UserPen className='text-secondary' /> {t('buttonWeb.profile')}
+                <UserPen className="text-secondary" /> {t("buttonWeb.profile")}
               </Button>
             </Link>
           </DropdownMenuItem>
-          <DropdownMenuItem className='px-2 font-semibold flex gap-3 '>
-            {' '}
+          <DropdownMenuItem className="px-2 font-semibold flex gap-3 ">
+            {" "}
             <Button
               onClick={handleSignOut}
-              variant={'link'}
-              className='px-0 flex gap-1 justify-start w-full text-base text-black justify-start hover:no-underline'
+              variant={"link"}
+              className="px-0 flex gap-1 justify-start w-full text-base text-black justify-start hover:no-underline"
             >
-              <LogOutIcon className='text-secondary' /> {t('buttonWeb.logout')}
+              <LogOutIcon className="text-secondary" /> {t("buttonWeb.logout")}
             </Button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-      <div className='absolute bg-slate-200 rounded-full flex justify-center items-center bottom-[-5px] right-[-5px]'>
-        <ChevronDown className='size-3 ' />
+      <div className="absolute bg-slate-200 rounded-full flex justify-center items-center bottom-[-5px] right-[-5px]">
+        <ChevronDown className="size-3 " />
       </div>
     </div>
   );
