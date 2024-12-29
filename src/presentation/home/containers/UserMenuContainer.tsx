3ce51ec422bf6c055f896e3669dbd9ed891a9f40
NguyenDucTin <99305494+Yonko08042002@users.noreply.github.com>
2024-12-30 03:34:06 +0700

    feat/master:fix type Checkout

--- src/presentation/home/containers/UserMenuContainer.tsx
+++ src/presentation/home/containers/UserMenuContainer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getMe } from "application/use-cases/user";
 import { getCartToursByUserId } from "application/use-cases/cart";
 import { UserMenu } from "shared/components/organisms/UserMenu";
